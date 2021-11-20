fe6d7b8d7059adbd4577852e9ba97a273825a2d3
dentaku <dentaku1412@gmail.com>
2021-11-20 18:53:26 +0900

    Add  option

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ cargo test
 
 docker build -t compilerbook .
 
-docker run compilerbook cargo run --release '23 - 8+5- 3'
-docker run compilerbook cargo run --release '23 - 8+5- 3 9 +'
+docker run --rm compilerbook cargo run --release '23 - 8+5- 3'
+docker run --rm compilerbook cargo run --release '23 - 8+5- 3 9 +'
